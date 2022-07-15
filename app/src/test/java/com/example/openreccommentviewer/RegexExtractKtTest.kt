bff05cf1b185c5324f3d4434ef4d94c3c27b3bdb
shigaresso <70837100+shigaresso@users.noreply.github.com>
2022-07-15 17:49:08 +0900

    動画 ID を抜き出すテストの記述

--- app/src/test/java/com/example/openreccommentviewer/RegexExtractKtTest.kt
+++ app/src/test/java/com/example/openreccommentviewer/RegexExtractKtTest.kt
@@ -7,8 +7,31 @@ class RegexExtractKtTest {
     @Test
     fun extractUserId() {
         // act
-        val actual = extractString(targetValue = "eCount\":-1}],\"channel\":{\"user\":{\"id\":\"sumomo_xqx\",\"userKey\"", extractPattern = """"channel":\{"user":\{"id":"(.+?)"""")
+        val actual = extractString(
+            targetValue = "eCount\":-1}],\"channel\":{\"user\":{\"id\":\"sumomo_xqx\",\"userKey\"",
+            extractPattern = """"channel":\{"user":\{"id":"(.+?)"""",
+        )
         // assert
         assertEquals("sumomo_xqx", actual)
+    }
+
+    @Test
+    fun extractMovieId() {
+        val target = """
+            [
+	            {
+		            "id": "o7z4e2x2q8l",
+		            "movie_id": 2713650,
+		            "title": "新作女の子部屋に憑き配信者に導く神ゲー「ノゾムキミノミライ」やるぞ！！",
+                }
+            ]
+        """
+        // act
+        val actual = extractString(
+            targetValue = target,
+            extractPattern = """movie_id": ([0-9]+)""",
+        )
+        // assert
+        assertEquals("2713650", actual)
     }
 }
