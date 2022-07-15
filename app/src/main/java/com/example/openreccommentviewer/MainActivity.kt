9fb899d0fd8436cdd7036008babce6c42c60f580
shigaresso <70837100+shigaresso@users.noreply.github.com>
2022-07-16 01:04:06 +0900

    UI を表示する Compose 関数が ViewModel を用いるようにした。
    これによって View に記述されていた、ボタンを押した時に実行する処理や状態などを ViewModel に記述でき、View は UI を表すことに専念できる。
    
    状態が必要な場合は、ViewModel で定義されている状態変数を用いる。
    ここで注意点が ViewModel インスタンスをコンストラクタで定義せず、内部で保持する形にすると、ViewModel で状態の値が変化した時に View 側で値の更新がされない。
    
    よって ViewModel は Compose のコンストラクタで記述するように注意する。

--- app/src/main/java/com/example/openreccommentviewer/MainActivity.kt
+++ app/src/main/java/com/example/openreccommentviewer/MainActivity.kt
@@ -13,11 +13,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.openreccommentviewer.ui.theme.OpenrecCommentViewerTheme
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.time.LocalDateTime
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,59 +32,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GetCommentButton() {
-    // Compose でコルーチンを使うため
-    val scope = rememberCoroutineScope()
-    var isConnecting by remember { mutableStateOf(false) }
+fun GetCommentButton(viewModel: MainViewModel = MainViewModel()) {
+
+    val isConnecting by viewModel.isConnecting.collectAsState()
+
     Column {
         Button(
             enabled = !isConnecting,
-            onClick = {
-                isConnecting = true
-                scope.launch(Dispatchers.IO) {
-                    val httpClient = HttpClient()
-                    val liveHtmlBody = httpClient.getHtmlBody(
-                        urlString = "https://www.openrec.tv/live/1o8qy6ld1zk"
-                    )
-                    val userId = withContext(Dispatchers.Default) {
-                        return@withContext extractString(
-                            targetValue = liveHtmlBody,
-                            extractPattern = """"channel":\{"user":\{"id":"(.+?)""""
-                        )
-                    }
-                    val apiHtmlBody = httpClient.getHtmlBody(
-                        urlString = "https://public.openrec.tv/external/api/v5/movies?channel_ids=$userId&sort=onair_status&is_upload=false"
-                    )
-                    val movieId = extractString(
-                            targetValue = apiHtmlBody,
-                            extractPattern = """movie_id":([0-9]+)"""
-                        )
-                    val client = WebSocketClient(movieId)
-                    while (isConnecting) {
-                        client.send()
-                        withContext(Dispatchers.Default) {
-                            println("25秒休みます: ${LocalDateTime.now()}")
-                            delay(25000)
-                        }
-                    }
-                }
-            },
+            onClick = { viewModel.connectLiveStream() },
         ) {
             Text(text = "Get Comment")
         }
 
         Button(
             enabled = isConnecting,
-            onClick = { isConnecting = false },
+            onClick = { viewModel.changeIsConnect() },
         ) {
             Text(text = "Disconnect")
         }
-
-    }
-}
-
-suspend fun sleep() {
-    withContext(Dispatchers.Default) {
 
     }
 }
