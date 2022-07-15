2990df896e29d18aedd5a7d6655b658c395cd96a
shigaresso <70837100+shigaresso@users.noreply.github.com>
2022-07-15 22:11:09 +0900

    WebSocket 接続するアドレスの必要部分を引数で受け取る方式に変えた

--- app/src/main/java/com/example/openreccommentviewer/WebSocketClient.kt
+++ app/src/main/java/com/example/openreccommentviewer/WebSocketClient.kt
@@ -8,14 +8,14 @@ import okhttp3.WebSocket
 import okhttp3.WebSocketListener
 
 // OkHttp3 を用いて WebSocket 接続を使うには継承が必要
-class WebSocketClient : WebSocketListener() {
+class WebSocketClient(movieId: String) : WebSocketListener() {
     private val ws: WebSocket
 
     init {
         val client = OkHttpClient()
 
         val request = Request.Builder()
-            .url("wss://chat.openrec.tv/socket.io/?movieId=2713447&uuid=655DF103-EB90-96BE-F7BB-305E1E8F094B&referrer=https%3A%2F%2Fwww.openrec.tv%2Flive%2Fnqr603w7gr6&connectAt=1657818667&connectionId=55e0772d-b661-40c2-8b42-f29d66e1dd8e&isExcludeLiveViewers=true&accessToken=dc6260a5-51f4-49f0-866a-81779a4c36bd&EIO=3&transport=websocket")
+            .url("wss://chat.openrec.tv/socket.io/?movieId=$movieId&EIO=3&transport=websocket")
             .build()
 
         ws = client.newWebSocket(request, this)
