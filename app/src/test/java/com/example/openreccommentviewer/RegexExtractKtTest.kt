82be1c1b7e39e131a955be63fcaf88d3335d4633
shigaresso <70837100+shigaresso@users.noreply.github.com>
2022-07-15 20:14:00 +0900

    テストする内容が間違っていたので修正
    テストに用いる文字列に JSON 整形ツールを使用すると、実際に入手する形式とは異なる文字列になっていた。
    JSON 形式でいう Key Value の間に半角スペースが入っていた
    
    誤 → "movie_id": 2713650
    正 → "movie_id":2713650
    
    よって今回のレベルの正規表現だと 誤 に合わせた正規表現を記述していたので、エラーが出た

--- app/src/test/java/com/example/openreccommentviewer/RegexExtractKtTest.kt
+++ app/src/test/java/com/example/openreccommentviewer/RegexExtractKtTest.kt
@@ -20,16 +20,16 @@ class RegexExtractKtTest {
         val target = """
             [
 	            {
-		            "id": "o7z4e2x2q8l",
-		            "movie_id": 2713650,
-		            "title": "新作女の子部屋に憑き配信者に導く神ゲー「ノゾムキミノミライ」やるぞ！！",
+		            "id":"o7z4e2x2q8l",
+		            "movie_id":2713650,
+		            "title":"新作女の子部屋に憑き配信者に導く神ゲー「ノゾムキミノミライ」やるぞ！！",
                 }
             ]
         """
         // act
         val actual = extractString(
             targetValue = target,
-            extractPattern = """movie_id": ([0-9]+)""",
+            extractPattern = """movie_id":([0-9]+)""",
         )
         // assert
         assertEquals("2713650", actual)
