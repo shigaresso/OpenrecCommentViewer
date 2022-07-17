8bfedbf48e7c3de599f94937c0ae53c7b7bfbc34
shigaresso <70837100+shigaresso@users.noreply.github.com>
2022-07-18 00:37:19 +0900

    WebSocket 接続をクラス内部で実行するように変更

--- app/src/main/java/com/example/openreccommentviewer/WebSocketClient.kt
+++ app/src/main/java/com/example/openreccommentviewer/WebSocketClient.kt
@@ -4,12 +4,27 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import okhttp3.WebSocket
 import okhttp3.WebSocketListener
 
 // OkHttp3 を用いて WebSocket 接続を使うには継承が必要
-class WebSocketClient : WebSocketListener() {
-    lateinit var ws: WebSocket
+class WebSocketClient(movieId: String) : WebSocketListener() {
+    private val ws: WebSocket
+
+    init {
+        val okHttpClient = OkHttpClient()
+        val request = Request.Builder()
+            .url("wss://chat.openrec.tv/socket.io/?movieId=$movieId&EIO=3&transport=websocket")
+            .build()
+        val webSocket = okHttpClient.newWebSocket(request, this)
+        ws = webSocket
+    }
+
+    fun disConnect() {
+        ws.close(1000, "")
+    }
 
     private val _commentData = MutableStateFlow("")
     val commentData: StateFlow<String> = _commentData
@@ -27,5 +42,9 @@ class WebSocketClient : WebSocketListener() {
     override fun onMessage(webSocket: WebSocket, text: String) {
         println("Received text message: $text")
         outputData(text)
+    }
+
+    override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
+        println("WebSocket 接続を終了しました")
     }
 }
